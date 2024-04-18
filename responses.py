3c6c4cff36fbdc8eb6d8407f8915bf03acfd0d0d
shrid <skanuparthy21@gmail.com>
2024-04-17 23:21:34 -0700

    Added !help

--- responses.py
+++ responses.py
@@ -8,7 +8,7 @@ def handle_response(message) -> str:
     #         gameinProgress = True
     #         return "Starting a game of Coup, react with :white_check_mark: to join. React with :bomb: to start without 6 players."
 
-    if p_message == '!rules':
+    if p_message == '!rules' or '!help':
         return "Type !start to initialize a game. Refer to https://boardgame.bg/coup%20rules%20pdf.pdf to learn coup game rules\nAt the beginning of the game, you will recieve a direct message of your 2 starting cards which are 2 emojis.\nThe 5 possible cards you may recieve are:\nDuke: 🤵\nAmbassador: 🧑‍✈️\nCaptain: 🦹‍♂️\nAssassin: 🥷\nContessa: 💃\n\nIn addition to this, at the start of each turn, you may do one of the following actions:\n🪙: Gain one coin\n💵: Gain 2 coins (can be blocked by someone claiming duke)\n💰: Claim duke to earn 3 coins\n😈: Claim captain to steal 2 coins from another player\n🔪: Claim assassin to pay 3 coins to assassinate another player\n🎲: Claim ambassador to swap your cards\n⚔️: Pay 7 coins to coup another player (You will automatically be forced to coup if you have 10 or more coins)\n\nWhenever there is a card claim, all players must unanimously vote to allow the claim by reacting with 👌\nOtherwise, the player who first presses 👊 will be the one to challenge the claim."
     if p_message == '!green fn':
         return 'https://tenor.com/view/green-fn-peter-griffin-green-gif-1521709978489846232'
